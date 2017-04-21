8b0ed01f754231ec304df79928774eed9fe1aa66
Vadym <quadropoison@users.noreply.github.com>
2017-04-21 18:20:51 +0300

    Initialize unit test project with first tests into it

---
+++ CompareTool.Tests/TextFilesComparatorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompareTool.Tests
+{
+    [TestClass]
+    public class TextFilesComparatorTests
+    {
+        [TestMethod]
+        public void CompareTwoStrings_ShouldReturn_AssertTrue()
+        {
+            bool compareResult = false;
+            var dataOne = "Test string ZeroZeroOne";
+
+            TextFilesComparator comparator = new TextFilesComparator();
+           
+            comparator.CompareFilesAsStrings(dataOne, dataOne);
+            compareResult = comparator.IsEquals;
+
+            Assert.IsTrue(compareResult);
+        }
+
+        [TestMethod]
+        public void CompareTwoStrings_ShouldReturn_AssertFalse()
+        {
+            bool compareResult = false;
+            var dataOne = "Test string ZeroZeroOne";
+            var dataTwo = "Test string ZeroZeroTwo";
+
+            TextFilesComparator comparator = new TextFilesComparator();
+           
+            comparator.CompareFilesAsStrings(dataOne, dataTwo);
+            compareResult = comparator.IsEquals;
+
+            Assert.IsFalse(compareResult);
+        }
+    }
+}
